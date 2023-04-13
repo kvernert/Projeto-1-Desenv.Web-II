a80056c6dc93189980c6d9dfae383bb112d9eb7b
kvernet <kverner47@gmail.com>
2023-04-13 19:00:30 -0300

    update pt.2

--- src/App.vue
+++ src/App.vue
@@ -14,111 +14,71 @@ const estado = ref('')
 const hobbies = ref('')
 const linguagensProg = ref('')
 const biografia = ref('')
-const perfil = ref('')
 const enviar = ref(false)
 const mensagemErro = ref('')
+const user = ref({avatar: null})
 
+function handleFileUpload(e) {
+  const target = e.target
+  if (target && target.files) {
+    const file = target.files[0]
+    user.value.avatar = URL.createObjectURL(file)
+  }
+}
 function confirmacao() {
   if (senha.value === confirmarSenha.value) {
     mensagemErro.value = ''
     return true
   } else {
-    mensagemErro.value = 'Senha e Confirmação de Senha não conferem!'
+    mensagemErro.value = 'As '
     return false
   }
 }
 </script>
 
 <template>
-  <div>
+  <div class="blo1">
     <form @submit.prevent="enviar = confirmacao()">
       <h1>Edição de Perfil</h1>
       <h3>Informações pessoais</h3>
       <div>
         <h3>Escolha sua nova foto de perfil</h3>
-        <input type="hidden" v-model="perfil" />
-        <input type="file" />
+        <input type="file" id="avatarField" @change="handleFileUpload($event)"/>
       </div>
       <div>
-        <input
-          type="text"
-          v-model="nome"
-          v-on:keypress="enviar = false"
-          placeholder="Digite seu nome..."
-          required
-        />
+        <input type="text" v-model="nome" v-on:keypress="enviar = false" placeholder="Digite seu nome..." required />
       </div>
       <div>
         <h4>Insira sua data de Nascimento:</h4>
         <input type="date" v-model="dataNascimento" v-on:keypress="enviar = false" required />
       </div>
       <div>
-        <input
-          type="email"
-          v-model="email"
-          v-on:keypress="enviar = false"
-          placeholder="Digite seu e-mail..."
-          required
-        />
+        <input type="email" v-model="email" v-on:keypress="enviar = false" placeholder="Digite seu e-mail..." required />
       </div>
       <hr />
       <h3>Informações de senha</h3>
       <div>
-        <input
-          type="password"
-          v-model="senha"
-          v-on:keypress="enviar = false"
-          placeholder="Digite sua senha..."
-          required
-        />
+        <input type="password" v-model="senha" v-on:keypress="enviar = false" placeholder="Digite sua senha..."
+          required />
       </div>
       <div>
-        <input
-          type="password"
-          v-model="confirmarSenha"
-          v-on:keypress="enviar = false"
-          placeholder="Confirme sua senha..."
-          required
-        />
+        <input type="password" v-model="confirmarSenha" v-on:keypress="enviar = false" placeholder="Confirme sua senha..."
+          required />
       </div>
       <hr />
       <h3>Informações de endereço</h3>
       <div>
-        <input
-          type="text"
-          v-model="rua"
-          v-on:keypress="enviar = false"
-          required
-          placeholder="Digite sua rua..."
-        />
+        <input type="text" v-model="rua" v-on:keypress="enviar = false" required placeholder="Digite sua rua..." />
       </div>
       <div>
-        <input
-          type="number"
-          v-model="numeroCasa"
-          v-on:keypress="enviar = false"
-          required
-          min="1"
-          placeholder="Número da sua casa..."
-        />
+        <input type="number" v-model="numeroCasa" v-on:keypress="enviar = false" required min="1"
+          placeholder="Número da sua casa..." />
       </div>
       <div>
-        <input
-          type="text"
-          v-model="bairro"
-          v-on:keypress="enviar = false"
-          required
-          placeholder="Digite seu bairro..."
-        />
+        <input type="text" v-model="bairro" v-on:keypress="enviar = false" required placeholder="Digite seu bairro..." />
       </div>
       <div>
-        <input
-          type="text"
-          v-model="cidade"
-          v-on:keypress="enviar = false"
-          required
-          placeholder="Digite sua cidade..."
-        />
+        <input type="text" v-model="cidade" v-on:keypress="enviar = false" required placeholder="Digite sua cidade..." />
       </div>
       <div>
         <h4>Insira seu estado:</h4>
@@ -158,8 +118,19 @@ function confirmacao() {
         <textarea v-model="hobbies" v-on:keypress="enviar = false"></textarea>
       </div>
       <div>
-        <h4>Diga quais são as suas linguagens de programação:</h4>
-        <textarea v-model="linguagensProg" v-on:keypress="enviar = false"></textarea>
+        <label for="linguagem">Diga quais são as suas linguagens de programação:</label>
+        <input type="checkbox" v-model="cores" value="PY" /> Python
+        <input type="checkbox" v-model="cores" value="C#" /> C#
+        <input type="checkbox" v-model="cores" value="C++" /> C++
+        <input type="checkbox" v-model="cores" value="C" /> C
+        <input type="checkbox" v-model="cores" value="JV" /> Java
+        <input type="checkbox" v-model="cores" value="JS" /> JavaScript
+        <input type="checkbox" v-model="cores" value="PHP" /> PHP
+        <input type="checkbox" v-model="cores" value="SW" /> Swift
+        <input type="checkbox" v-model="cores" value="Go" /> Go
+        <input type="checkbox" v-model="cores" value="SQL" /> SQL
+        <input type="checkbox" v-model="cores" value="RB" /> Ruby
+        <input type="checkbox" v-model="cores" value="TS" /> TypeScript
       </div>
       <div>
         <h4>Insira uma Biografia:</h4>
@@ -172,7 +143,7 @@ function confirmacao() {
 
   <div v-if="enviar">
     <h1>Perfil Atualizado:</h1>
-    <div><img src="{{ perfil }}" alt="" /></div>
+    <div><img :src="user.avatar" /></div>
     <div>
       <p>{{ nome }}</p>
     </div>
@@ -185,13 +156,24 @@ function confirmacao() {
     <div></div>
     <div>
       <p>
-        Endereço: {{ endereco_rua }}, {{ endereco_numero }}, {{ endereco_bairro }}, {{ cidade }},
+        Endereço: {{ rua }}, {{ numeroCasa }}, {{ bairro }}, {{ cidade }},
         {{ estado }}
       </p>
     </div>
-    <div class="hoobie_atualizado">Hoobie(s): {{ hobbies }}</div>
-    <div class="ling_atualizada">Linguagem(s) de Programação: {{ ling_programacao }}</div>
-    <div class="bio-atualizada">Biografia: {{ biografia }}</div>
+    <div>Hoobie(s): {{ hobbies }}</div>
+    <div>Linguagem(s) de Programação: {{ linguagensProg }}</div>
+    <div>Biografia: {{ biografia }}</div>
   </div>
 </template>
-<style scoped></style>
+<style scoped>
+.blo1{
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+}
+#avatarField{
+
+}
+</style>
